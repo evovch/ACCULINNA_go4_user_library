bb7f76272891f2ddc76816420961022935075c75
evovch <evovch@gmail.com>
2018-09-05 14:58:00 +0300

    Add a not very nice but easy solution to modify LD_LIBRARY_PATH inside env.sh

--- env.sh
+++ env.sh
@@ -9,3 +9,8 @@ cd -
 cd ${SOFTDIR}/FairSoft_install/bin/
 . ./thisroot.sh
 cd -
+
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+export LD_LIBRARY_PATH=${DIR}/install/lib:$LD_LIBRARY_PATH
